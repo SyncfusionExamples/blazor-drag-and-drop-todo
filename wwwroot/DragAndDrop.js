b1b27ac9da2781f4648b8ed08cb070a6a6797449
Rajendran R <rajendranr-5483@users.noreply.github.com>
2022-09-12 06:24:45 +0530

    Modified a sample

--- wwwroot/DragAndDrop.js
+++ wwwroot/DragAndDrop.js
@@ -6,7 +6,7 @@ function DragStart(args, id) {
 
 function DropCard(args) {
     var dropElement = document.elementFromPoint(args.clientX, args.clientY);
-    var IsCompleted = dropElement.closest('.e-accordion') ? true : false;
+    var IsDroppedOnCompletedPane = dropElement.closest('.e-accordion') ? true : false;
     var dropCard = dropElement.closest('.taskitem');
-    DotNet.invokeMethodAsync('Blazor_WASM_Drag_and_Drop', 'UpdateCards', IsCompleted, dragCardId, dropCard != null ? dropCard.id : null);
+    DotNet.invokeMethodAsync('Blazor_WASM_Drag_and_Drop', 'UpdateCards', IsDroppedOnCompletedPane, dragCardId, dropCard != null ? dropCard.id : null);
 }
