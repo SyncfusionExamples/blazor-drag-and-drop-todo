be7561539d2ae3770168e3762fe2134dbe32fbce
Backiaraj <backiaraj.kanagaraj@syncfusion.com>
2024-02-07 18:41:09 +0530

    Update the project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
-﻿using Blazor_WASM_Drag_and_Drop;
+using DragDrop;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
-
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzA4NDIzMUAzMjM0MmUzMDJlMzBDazZwNldXQ0dzWlhyL0x4VkZib0Urd3ZUcmJTN29WSlhMNlpEUWk0UnlJPQ==");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSyncfusionBlazor();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
